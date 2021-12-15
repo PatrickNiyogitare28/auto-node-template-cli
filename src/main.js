d1f6c684b63217e6789cdb81fd6822a8f3f438d5
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2021-12-15 14:56:43 +0200

    chore: added copy files

---
+++ src/main.js
@@ -0,0 +1,43 @@
+import chalk from "chalk";
+import fs from 'fs';
+import path from "path";
+import ncp from "ncp";
+import { promisify } from "util";
+
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+async function copyTemplateFiles() {
+  return copy(options.templateDirectory, options.targetDirectory, {
+      clobber: false,
+  });
+}
+
+export async function crateProject(options){
+    options = {
+        ...options,
+        targetDirectory: options.targetDirectory || process.cwd(),
+    };
+
+    const currentFileUrl = import.meta.url;
+    const templateDir =     path.resolve(
+        new URL(currentFileUrl).pathname,
+        '../../templates',
+        options.template.toLowerCase()
+    );
+    options.templateDirectory = templateDir;
+
+    try{
+        await access(templateDir, fs.constants.R_OK);
+    }
+    catch(err){
+        console.error('%s Invalid template name', chalk.red.bold('ERROR'));
+        process.exit(1);
+    }
+
+    console.log('Copy project files');
+    await copyTemplateFiles(options);
+
+    console.log('%s Project ready', chalk.green.bold('DONE'));
+    return true;   
+}
