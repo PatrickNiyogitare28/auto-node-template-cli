2e5e70185e13db817b4119b638963d138b572905
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2021-12-16 21:30:05 +0200

    chore(development): added install packages

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
 import { promisify } from 'util';
+import execa from 'execa';
+import Listr from 'listr';
+import { projectInstall } from 'pkg-install';
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -13,26 +16,56 @@ async function copyTemplateFiles(options) {
  });
 }
 
+async function initGit(options) {
+ const result = await execa('git', ['init'], {
+   cwd: options.targetDirectory,
+ });
+ if (result.failed) {
+   return Promise.reject(new Error('Failed to initialize git'));
+ }
+ return;
+}
+
 export async function createProject(options) {
  options = {
    ...options,
-   targetDirectory: options.targetDirectory || process.cwd(),
+   targetDirectory: options.targetDirectory || process.cwd()
  };
 
-const templateDirUrl =  `${path.dirname(path.resolve(__dirname))}\\templates`
-options.templateDirectory = `${templateDirUrl}\\${options.template.toLowerCase()}`;
+ const templateDirUrl =  `${path.dirname(path.resolve(__dirname))}\\templates`
+ options.templateDirectory = `${templateDirUrl}\\${options.template.toLowerCase()}`;
 
  try {
    await access(templateDirUrl, fs.constants.R_OK);
  } catch (err) {
-   console.error('%s Invalid template name', kleur.red().bold('ERROR'));
-   console.log(err)
+   console.error('%s Invalid template name',  kleur.red().bold('ERROR'));
    process.exit(1);
  }
 
- console.log('Copy project files');
- await copyTemplateFiles(options);
+ const tasks = new Listr([
+   {
+     title: 'Copy project files',
+     task: () => copyTemplateFiles(options),
+   },
+   {
+     title: 'Initialize git',
+     task: () => initGit(options),
+     enabled: () => options.git,
+   },
+   {
+     title: 'Install dependencies',
+     task: () =>
+       projectInstall({
+         cwd: options.targetDirectory,
+       }),
+     skip: () =>
+       !options.runInstall
+         ? 'Pass --install to automatically install dependencies'
+         : undefined,
+   },
+ ]);
 
- console.log('%s Project ready', kleur.green().bold('DONE'));
+ await tasks.run();
+ console.log('%s Project ready',kleur.green().bold('DONE'));
  return true;
 }
